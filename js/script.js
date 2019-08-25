fac23888d6505155653054c8ecca7e4d9f8de4ef
Anna Wroblewska <a.j.wroblewska@gmail.com>
2019-08-25 16:53:12 +0200

    add restar button and scroll

--- js/script.js
+++ js/script.js
@@ -1,6 +1,22 @@
 var elem = document.querySelector('.main-carousel');
 var flkty = new Flickity(elem, {
-    // options
+
     cellAlign: 'left',
     contain: true
+});
+
+var restart = document.querySelector('.restart');
+
+
+restart.addEventListener("click", function (event) {
+
+    flkty.selectCell(document.querySelector(".initial"));
+});
+
+
+var progressBar = document.querySelector('.progress-bar')
+
+flkty.on('scroll', function (progress) {
+    progress = Math.max(0, Math.min(1, progress));
+    progressBar.style.width = progress * 100 + '%';
 });
