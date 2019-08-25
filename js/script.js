ee44ebec32cb2df7a79fdad9800d1b467e5b2f42
Anna Wroblewska <a.j.wroblewska@gmail.com>
2019-08-26 00:24:39 +0200

    fix hash

--- js/script.js
+++ js/script.js
@@ -2,7 +2,8 @@ var elem = document.querySelector('.main-carousel');
 var flkty = new Flickity(elem, {
 
     cellAlign: 'left',
-    contain: true
+    contain: true,
+    hash: true
 });
 
 var restart = document.querySelector('.restart');
